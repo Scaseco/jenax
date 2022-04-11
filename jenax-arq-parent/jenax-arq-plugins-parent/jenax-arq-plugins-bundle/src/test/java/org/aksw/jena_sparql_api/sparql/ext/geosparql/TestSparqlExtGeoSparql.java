267f708c488ce5dd8628cb272378e9440d3e6e9f
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2022-04-11 13:01:57 +0200

    as GeoJSON function

--- jenax-arq-parent/jenax-arq-plugins-parent/jenax-arq-plugins-bundle/src/test/java/org/aksw/jena_sparql_api/sparql/ext/geosparql/TestSparqlExtGeoSparql.java
+++ jenax-arq-parent/jenax-arq-plugins-parent/jenax-arq-plugins-bundle/src/test/java/org/aksw/jena_sparql_api/sparql/ext/geosparql/TestSparqlExtGeoSparql.java
@@ -3,6 +3,8 @@ package org.aksw.jena_sparql_api.sparql.ext.geosparql;
 import org.apache.jena.geosparql.implementation.GeometryWrapper;
 import org.apache.jena.geosparql.implementation.vocabulary.GeoSPARQL_URI;
 import org.apache.jena.geosparql.implementation.vocabulary.SRS_URI;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.shared.PrefixMapping;
 import org.apache.jena.shared.impl.PrefixMappingImpl;
 import org.apache.jena.sparql.expr.NodeValue;
@@ -11,6 +13,11 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.google.common.math.DoubleMath;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.LineString;
+import org.locationtech.jts.io.ParseException;
+import org.locationtech.jts.io.WKTReader;
+import org.locationtech.jts.io.geojson.GeoJsonReader;
 
 
 public class TestSparqlExtGeoSparql {
@@ -64,6 +71,27 @@ public class TestSparqlExtGeoSparql {
 		Assert.assertTrue(DoubleMath.fuzzyEquals(1, GeoFunctionsJena.y(gw.getParsingGeometry()), TOLERANCE));
 		
 //		System.out.println(gw);
+	}
+
+	@Test
+	public void testAsGeoJSON() throws ParseException {
+		PrefixMapping pm = new PrefixMappingImpl();
+		pm.setNsPrefixes(GeoSPARQL_URI.getPrefixes());
+
+		String queryStr =
+				"SELECT ?g { BIND(fn:asGeoJSON('<gml:LineString srsName=\"EPSG:25832\"><gml:coordinates>663957.75944074022118,5103981.64908889029175 663955.915655555087142,5103991.151674075052142</gml:coordinates></gml:LineString>') AS ?g) }";
+		String[] tmpActual = {null};
+		QueryExecutionFactory.create(queryStr, ModelFactory.createDefaultModel()).execSelect()
+				.forEachRemaining(
+						qs -> tmpActual[0] = qs.get("g").asNode().getLiteralLexicalForm());
+		String actual = tmpActual[0];
+		String expected =
+				"LINESTRING (11.12011600889509 46.06974332787273, 11.120095457435852 46.06982923685294)";
+		GeometryFactory geometryFactory = new GeometryFactory();
+		GeoJsonReader reader = new GeoJsonReader(geometryFactory);
+		LineString expectedGeo = (LineString) reader.read(expected);
+		LineString actualGeo = (LineString) reader.read(tmpActual[0]);
+		Assert.assertEquals(true, expectedGeo.equalsExact(actualGeo, TOLERANCE));
 	}
 
 //	@Test
