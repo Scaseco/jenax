9950be903f81b61b7d4377fd6b5d69c35316a55d
Simon Bin <sbin@informatik.uni-leipzig.de>
2022-09-09 22:53:26 +0200

    rename non-std union

--- jenax-arq-parent/jenax-arq-plugins-parent/jenax-arq-plugins-bundle/src/main/java/org/aksw/jena_sparql_api/sparql/ext/geosparql/GeoSparqlExFunctions.java
+++ jenax-arq-parent/jenax-arq-plugins-parent/jenax-arq-plugins-bundle/src/main/java/org/aksw/jena_sparql_api/sparql/ext/geosparql/GeoSparqlExFunctions.java
@@ -64,7 +64,7 @@ public class GeoSparqlExFunctions {
         return GeometryWrapperUtils.createFromPrototype(geom, tmp);
     }
 
-    @IriNs(GeoSPARQL_URI.GEOF_URI)
+    @IriNs(GeoSPARQL_URI.SPATIAL_FUNCTION_URI)
     public static GeometryWrapper union(GeometryWrapper geom) {
         // List<Geometry> list = expandCollection(geom.getParsingGeometry()).collect(Collectors.toList());
         GeometryWrapper result = GeometryWrapperUtils.createFromPrototype(geom,
