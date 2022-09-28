4f7f39471d74a1515578b4789b932783bc21682b
Claus Stadler <RavenArkadon@googlemail.com>
2022-09-29 01:46:35 +0200

    Updated QueryEngineFactoryFromAsFilter to use altered DynamicDatasetGraph

--- jenax-arq-parent/jenax-arq-fromasfilter/src/main/java/org/aksw/jenax/arq/fromasfilter/engine/QueryEngineFactoryFromAsFilter.java
+++ jenax-arq-parent/jenax-arq-fromasfilter/src/main/java/org/aksw/jenax/arq/fromasfilter/engine/QueryEngineFactoryFromAsFilter.java
@@ -2,9 +2,11 @@ package org.aksw.jenax.arq.fromasfilter.engine;
 
 import org.aksw.jenax.arq.fromasfilter.dataset.DatasetGraphFromAsFilter;
 import org.aksw.jenax.arq.util.syntax.ElementTransformDatasetDescription;
+import org.apache.jena.atlas.lib.Pair;
 import org.apache.jena.query.Query;
 import org.apache.jena.sparql.algebra.Op;
 import org.apache.jena.sparql.core.DatasetGraph;
+import org.apache.jena.sparql.core.DynamicDatasets.DynamicDatasetGraph;
 import org.apache.jena.sparql.engine.Plan;
 import org.apache.jena.sparql.engine.QueryEngineFactory;
 import org.apache.jena.sparql.engine.QueryEngineRegistry;
@@ -18,21 +20,49 @@ public class QueryEngineFactoryFromAsFilter
 {
     private static final Logger logger = LoggerFactory.getLogger(QueryEngineFactoryFromAsFilter.class);
 
-    @Override
-    public boolean accept(Query query, DatasetGraph dataset, Context context) {
-        return dataset instanceof DatasetGraphFromAsFilter;
+    public static DatasetGraph unwrapDynamicDataset(DatasetGraph dataset) {
+        return dataset instanceof DynamicDatasetGraph
+                ? ((DynamicDatasetGraph)dataset).getProjectedDataset()
+                : dataset;
     }
 
     @Override
+    public boolean accept(Query query, DatasetGraph dataset, Context context) {
+        return unwrapDynamicDataset(dataset) instanceof DatasetGraphFromAsFilter;
+    }
+
+    public static Pair<Query, DatasetGraph> unwrapDynamicDataset(Query query, DatasetGraph dataset) {
+        Pair<Query, DatasetGraph> result;
+        if (dataset instanceof DynamicDatasetGraph) {
+            DynamicDatasetGraph ddg = (DynamicDatasetGraph)dataset;
+            Query copy = query.cloneQuery();
+            copy.getGraphURIs().clear();
+            copy.getNamedGraphURIs().clear();
+            ddg.getDefaultGraphs().forEach(n -> copy.getGraphURIs().add(n.getURI()));
+            ddg.getNamedGraphs().forEach(n -> copy.getNamedGraphURIs().add(n.getURI()));
+
+            result = Pair.create(copy, ddg.getProjectedDataset());
+        } else {
+            result = Pair.create(query, dataset);
+        }
+        return result;
+    }
+
+
+    @Override
     public Plan create(Query query, DatasetGraph dataset, Binding inputBinding, Context context) {
-        DatasetGraphFromAsFilter wrapper = (DatasetGraphFromAsFilter)dataset;
+        Pair<Query, DatasetGraph> effective = unwrapDynamicDataset(query, dataset);
+        Query effectiveQuery = effective.getLeft();
+        DatasetGraph affectiveDataset = effective.getRight();
+
+        DatasetGraphFromAsFilter wrapper = (DatasetGraphFromAsFilter)affectiveDataset;
         DatasetGraph base = wrapper.getBase();
 
-        Query rewrittenQuery = ElementTransformDatasetDescription.rewrite(query);
+        Query rewrittenQuery = ElementTransformDatasetDescription.rewrite(effectiveQuery);
 
         // Switch to debug?
         if (logger.isInfoEnabled()) {
-            logger.info("Transformed FROM (NAMED) clauses to filters: " + rewrittenQuery);
+            logger.info("Transformed FROM (NAMED) clauses to filters:\n" + rewrittenQuery);
         }
 
         QueryEngineFactory qef = QueryEngineRegistry.findFactory(rewrittenQuery, base, context);
@@ -48,5 +78,4 @@ public class QueryEngineFactoryFromAsFilter
     public Plan create(Op op, DatasetGraph dataset, Binding inputBinding, Context context) {
         throw new UnsupportedOperationException();
     }
-
 }
