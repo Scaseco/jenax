fb8a108152793c37cf68b8230f5ea632e8ebe357
Claus Stadler <RavenArkadon@googlemail.com>
2022-12-16 15:58:56 +0100

    Fixed build (apparently regression in init of jena 4.7.0 - ARQ.getContext() may return null)

--- jenax-arq-parent/jenax-arq-plugins-parent/jenax-arq-plugins-bundle/src/main/java/org/aksw/jena_sparql_api/sparql/ext/url/E_UrlFetch.java
+++ jenax-arq-parent/jenax-arq-plugins-parent/jenax-arq-plugins-bundle/src/main/java/org/aksw/jena_sparql_api/sparql/ext/url/E_UrlFetch.java
@@ -9,17 +9,12 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 import java.util.regex.Pattern;
 
 import org.aksw.commons.beans.model.PropertyUtils;
+import org.aksw.commons.util.gson.GsonUtils;
 import org.aksw.jena_sparql_api.sparql.ext.json.JenaJsonUtils;
 import org.aksw.jena_sparql_api.sparql.ext.json.RDFDatatypeJson;
 import org.apache.jena.sparql.expr.ExprEvalException;
@@ -31,10 +26,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.io.CharStreams;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
 
 /**
  * url:fetch(?url, [option1 [, ... [, optionN]]])
@@ -176,132 +169,5 @@ public class E_UrlFetch
                 c.setRequestProperty(e.getKey(), e.getValue());
             }
         }
-    }
-}
-
-class UrlFetchSpec {
-    String url;
-    String method;
-    Map<String, String> headers;
-    String body;
-    Integer connectTimeout;
-    Integer readTimeout;
-
-    public String getUrl() {
-        return url;
-    }
-    public String getMethod() {
-        return method;
-    }
-    public Map<String, String> getHeaders() {
-        return headers;
-    }
-    public String getBody() {
-        return body;
-    }
-    public Integer getConnectTimeout() {
-        return connectTimeout;
-    }
-    public Integer getReadTimeout() {
-        return readTimeout;
-    }
-    @Override
-    public String toString() {
-        return "UrlFetchSpec [url=" + url + ", method=" + method + ", headers=" + headers + ", body=" + body
-                + ", connectTimeout=" + connectTimeout + ", readTimeout=" + readTimeout + "]";
-    }
-}
-
-class GsonUtils {
-
-    /** Set a string at a given path. Creates intermediate json objects as needed. A null value creates the path but removes the last key. */
-    public static JsonObject setString(JsonObject obj, List<String> keys, String value) {
-        return setElement(obj, keys, value == null ? null : new JsonPrimitive(value));
-    }
-
-    public static JsonObject setNumber(JsonObject obj, List<String> keys, Number number) {
-        return setElement(obj, keys, number == null ? null : new JsonPrimitive(number));
-    }
-
-    /** Set an element at a given path. Creates intermediate json objects as needed. A null value creates the path but removes the last key. */
-    public static JsonObject setElement(JsonObject obj, List<String> keys, JsonElement value) {
-        int l = keys.size();
-        JsonObject tgt = l == 0 ? obj : makePath(obj, keys.subList(0, l - 1));
-        String key = keys.get(l - 1);
-        if (value == null) {
-            tgt.remove(key);
-        } else {
-            tgt.add(key, value);
-        }
-        return obj;
-    }
-
-    /** Creates fresh empty JSON objects if a key has no value */
-    public static JsonObject makePath(JsonObject obj, String ...keys) {
-        return makePath(obj, Arrays.asList(keys));
-    }
-
-    public static JsonObject makePath(JsonObject obj, List<String> keys) {
-        for (String key : keys) {
-            JsonElement member = obj.get(key);
-
-            if (member == null) {
-                member = new JsonObject();
-                obj.add(key, member);
-            } else if (!(member instanceof JsonObject)) {
-                throw new RuntimeException("Encountered unexpected existing non-object member.");
-            }
-
-            obj = (JsonObject)member;
-        }
-
-        return obj;
-    }
-
-    /** This method is a stub. Currently it only splits by '.'. It should later
-     * also support array access syntax such as foo.bar["baz"] */
-    public static List<String> parsePathSegments(String str) {
-        return new ArrayList<>(Arrays.asList(str.split("\\.")));
-    }
-
-    public static JsonElement merge(JsonElement a, JsonElement b) {
-        JsonElement result;
-        if (a == null) {
-            result = b == null ? null : b.deepCopy();
-        } else if (b == null) {
-            result = a;
-        } else if (a.isJsonArray() && b.isJsonArray()) {
-            result = merge(a.getAsJsonArray(), b.getAsJsonArray());
-        } else if (a.isJsonObject() && b.isJsonObject()) {
-            result = merge(a.getAsJsonObject(), b.getAsJsonObject());
-        } else {
-            result = b;
-        }
-        return result;
-    }
-
-    public static JsonElement merge(JsonObject a, JsonObject b) {
-        for (Entry<String, JsonElement> e : b.entrySet()) {
-            String k = e.getKey();
-            JsonElement x = a.get(k);
-            JsonElement y = e.getValue();
-            JsonElement elt = merge(x, y);
-            a.add(k, elt);
-        }
-        return a;
-    }
-
-    public static JsonArray merge(JsonArray a, JsonArray b) {
-        Set<JsonElement> set = new HashSet<>(a.size());
-        a.forEach(set::add);
-        for (Iterator<JsonElement> it = b.iterator(); it.hasNext();) {
-            JsonElement item = it.next();
-            if (!set.contains(item)) {
-                set.add(item);
-                // Ensure the merged data is independent of b - therefore deep copy!
-                a.add(item.deepCopy());
-            }
-        }
-        return a;
     }
 }
