6c91bfa0afc83a02a3b0aa82042d18fef8b6267b
Claus Stadler <RavenArkadon@googlemail.com>
2023-01-23 18:18:51 +0100

    Fixed some bugs with PF_Range

--- jenax-arq-parent/jenax-arq-plugins-parent/jenax-arq-plugins-bundle/src/main/java/org/aksw/jena_sparql_api/sparql/ext/number/PF_Range.java
+++ jenax-arq-parent/jenax-arq-plugins-parent/jenax-arq-plugins-bundle/src/main/java/org/aksw/jena_sparql_api/sparql/ext/number/PF_Range.java
@@ -12,17 +12,18 @@ import org.apache.jena.sparql.engine.ExecutionContext;
 import org.apache.jena.sparql.engine.QueryIterator;
 import org.apache.jena.sparql.engine.binding.Binding;
 import org.apache.jena.sparql.engine.binding.BindingFactory;
-import org.apache.jena.sparql.engine.iterator.QueryIterNullIterator;
 import org.apache.jena.sparql.engine.iterator.QueryIterPlainWrapper;
-import org.apache.jena.sparql.engine.iterator.QueryIterSingleton;
 import org.apache.jena.sparql.expr.ExprEvalException;
 import org.apache.jena.sparql.expr.NodeValue;
 import org.apache.jena.sparql.pfunction.PropFuncArg;
 import org.apache.jena.sparql.pfunction.PropertyFunctionBase;
+import org.apache.jena.sparql.util.IterLib;
 
 /**
  * Generate (big) integer numbers for the specified range start (inclusive) and end (exclusive). The default increment is 1 but can be optionally set
  * to a different value.
+ *
+ * Stepping must always be a positive value - 0 or negative values return a QueryIterNull.
  *
  * Examples:
  * <pre>
@@ -58,32 +59,41 @@ public class PF_Range
         long end = BindingUtils.getNumber(binding, subjects.get(1)).longValue();
         long step = BindingUtils.tryGetNumber(binding, sn > 2 ? subjects.get(2) : null).map(Number::longValue).orElse(1l);
 
+        long rangeDelta = end - start;
+
         QueryIterator result;
 
-        // Catch the case where the sign of the step differs from that of (end - start)
-        // Attempting to fetch e.g. the range [0, 10) with an increment of -1 would otherwise hang indefinitely
-        int rangeDir = Long.signum(end - start);
         long stepDir = Long.signum(step);
 
-        if (rangeDir != stepDir) {
-            result = QueryIterNullIterator.create(execCxt);
+        if (stepDir <= 0) {
+            result = IterLib.noResults(execCxt);
         } else {
 
             Node outputNode = objects.get(0);
 
             Number outputValue = BindingUtils.getNumberNullable(binding, outputNode);
             if (outputValue != null) {
-                // Check if the outputValue is within range
+                // Check if the outputValue is part of the sequence
+                // start + step * n = output
+                // n = (output - start) / step | with n must be integer - this means modulo must be 0
                 long tmp = outputValue.longValue();
-                boolean isInRange = tmp >= start && tmp < end;
 
-                result = isInRange
-                    ? QueryIterSingleton.create(binding, execCxt)
-                    : QueryIterNullIterator.create(execCxt);
+                boolean isMultipleOfStep = (tmp - start) % step == 0;
+                boolean isInRange = rangeDelta >= 0
+                        ? tmp >= start && tmp < end
+                        : tmp <= start && tmp > end;
+
+                result = isInRange && isMultipleOfStep
+                    ? IterLib.result(binding, execCxt)
+                    : IterLib.noResults(execCxt);
 
             } else {
                 Var outputVar = (Var)outputNode;
-                Iterator<Binding> it = LongStream.iterate(start, x -> x < end, x -> x + step)
+                LongStream base = rangeDelta >= 0
+                        ? LongStream.iterate(start, x -> x < end, x -> x + step)
+                        : LongStream.iterate(start, x -> x > end, x -> x - step);
+
+                Iterator<Binding> it = base
                         .mapToObj(v -> BindingFactory.binding(binding, outputVar, NodeValue.makeInteger(v).asNode()))
                         .iterator();
                 result = QueryIterPlainWrapper.create(it, execCxt);
