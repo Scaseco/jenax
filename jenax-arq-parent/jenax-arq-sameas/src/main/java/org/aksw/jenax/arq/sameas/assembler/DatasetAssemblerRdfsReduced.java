6ba6fee3e3eabd1f2959f64ebab73c1fb63b0ca8
Claus Stadler <RavenArkadon@googlemail.com>
2023-02-02 23:50:18 +0100

    Added service plugins for SERVICE <sameAs:> <sameAs+rdfs:> and <rdfs:>

--- jenax-arq-parent/jenax-arq-sameas/src/main/java/org/aksw/jenax/arq/sameas/assembler/DatasetAssemblerRdfsReduced.java
+++ jenax-arq-parent/jenax-arq-sameas/src/main/java/org/aksw/jenax/arq/sameas/assembler/DatasetAssemblerRdfsReduced.java
@@ -6,21 +6,27 @@ import org.aksw.jenax.arq.util.dataset.DatasetGraphRDFSReduced;
 import org.apache.jena.assembler.Assembler;
 import org.apache.jena.assembler.exceptions.AssemblerException;
 import org.apache.jena.graph.Graph;
+import org.apache.jena.graph.Node;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.ResourceFactory;
-import org.apache.jena.rdfs.DatasetGraphRDFS;
 import org.apache.jena.rdfs.RDFSFactory;
 import org.apache.jena.rdfs.SetupRDFS;
 import org.apache.jena.rdfs.assembler.DatasetRDFSAssembler;
 import org.apache.jena.rdfs.assembler.VocabRDFS;
+import org.apache.jena.rdfs.setup.ConfigRDFS;
 import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.sparql.SystemARQ;
 import org.apache.jena.sparql.core.DatasetGraph;
 import org.apache.jena.sparql.core.assembler.DatasetAssembler;
+import org.apache.jena.sparql.util.Symbol;
 
 /**
  * Same as {@link DatasetRDFSAssembler} except that the type is jxp:DatasetRDFS
  */
 public class DatasetAssemblerRdfsReduced extends DatasetAssembler {
+
+    // Symbol for putting the RDFS T-BOX into a dataset's context (for Nodes rather than NodeIDs)
+    public static final Symbol symSetupRdfsNode = SystemARQ.allocSymbol("setupRdfsNode");
 
     public static final Resource TYPE = ResourceFactory.createResource(SameAsTerms.NS + "DatasetRDFS");
 
@@ -40,7 +46,9 @@ public class DatasetAssemblerRdfsReduced extends DatasetAssembler {
             throw new AssemblerException(root, "Required property missing: "+VocabRDFS.pRdfsSchemaFile) ;
 
         Graph schema = RDFDataMgr.loadGraph(schemaFile);
-        SetupRDFS setup = RDFSFactory.setupRDFS(schema);
+        ConfigRDFS<Node> setup = RDFSFactory.setupRDFS(schema);
+        base.getContext().set(symSetupRdfsNode, setup);
+
         DatasetGraph dsg = DatasetGraphRDFSReduced.wrap(base, setup);
         return dsg;
     }
--- jenax-arq-parent/jenax-arq-sameas/src/main/java/org/aksw/jenax/arq/sameas/init/SameAsInit.java
+++ jenax-arq-parent/jenax-arq-sameas/src/main/java/org/aksw/jenax/arq/sameas/init/SameAsInit.java
@@ -1,23 +1,28 @@
 package org.aksw.jenax.arq.sameas.init;
 
+import java.util.Objects;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.aksw.jenax.arq.sameas.assembler.DatasetAssemblerRdfsReduced;
 import org.aksw.jenax.arq.sameas.assembler.DatasetAssemblerSameAs;
 import org.aksw.jenax.arq.sameas.assembler.SameAsVocab;
-import org.aksw.jenax.arq.sameas.dataset.DatasetGraphSameAsOld;
 import org.aksw.jenax.arq.sameas.model.SameAsConfig;
 import org.aksw.jenax.arq.uniondefaultgraph.assembler.DatasetAssemblerUnionDefaultGraph;
 import org.aksw.jenax.arq.uniondefaultgraph.assembler.UnionDefaultGraphVocab;
+import org.aksw.jenax.arq.util.dataset.DatasetGraphRDFSReduced;
+import org.aksw.jenax.arq.util.dataset.DatasetGraphSameAs;
 import org.aksw.jenax.arq.util.dataset.DatasetGraphUnionDefaultGraph;
 import org.aksw.jenax.arq.util.exec.QueryExecUtils;
 import org.aksw.jenax.reprogen.core.JenaPluginUtils;
 import org.apache.jena.assembler.Assembler;
 import org.apache.jena.assembler.assemblers.AssemblerGroup;
+import org.apache.jena.rdfs.SetupRDFS;
 import org.apache.jena.sparql.core.DatasetGraph;
 import org.apache.jena.sparql.core.assembler.AssemblerUtils;
 import org.apache.jena.sparql.core.assembler.DatasetAssembler;
+import org.apache.jena.sparql.engine.ExecutionContext;
 import org.apache.jena.sparql.engine.QueryIterator;
 import org.apache.jena.sparql.service.ServiceExecutorRegistry;
 import org.apache.jena.sys.JenaSubsystemLifecycle;
@@ -39,21 +44,34 @@ public class SameAsInit
         JenaPluginUtils.registerResourceClasses(SameAsConfig.class);
         registerWith(Assembler.general);
 
-        String serviceName = "sameAs";
-        Pattern sameAsPrefix = Pattern.compile("^" + Pattern.quote(serviceName) + "($|:)");
+        registerServiceWrapper("sameAs", execCxt ->
+            DatasetGraphSameAs.wrap(DatasetGraphUnionDefaultGraph.wrapIfNeeded(execCxt.getDataset())));
+
+        registerServiceWrapper("sameAs+rdfs", execCxt -> {
+            SetupRDFS setup = Objects.requireNonNull(execCxt.getContext().get(DatasetAssemblerRdfsReduced.symSetupRdfsNode), "No RDFS setup found in the active dataset context");
+            return DatasetGraphRDFSReduced.wrap(DatasetGraphSameAs.wrap(DatasetGraphUnionDefaultGraph.wrapIfNeeded(execCxt.getDataset())), setup);
+        });
+
+        registerServiceWrapper("rdfs", execCxt -> {
+            SetupRDFS setup = Objects.requireNonNull(execCxt.getContext().get(DatasetAssemblerRdfsReduced.symSetupRdfsNode), "No RDFS setup found in the active dataset context");
+            return DatasetGraphRDFSReduced.wrap(DatasetGraphUnionDefaultGraph.wrapIfNeeded(execCxt.getDataset()), setup);
+        });
+    }
+
+    public static void registerServiceWrapper(String serviceName, Function<ExecutionContext, ? extends DatasetGraph> wrapper) {
+        Pattern servicePattern = Pattern.compile("^" + Pattern.quote(serviceName) + "($|:)");
         ServiceExecutorRegistry.get().addSingleLink(
             (opExec, opOrig, binding, execCxt, chain) -> {
                 QueryIterator r;
                 String uri = opExec.getService().getURI();
-                Matcher m = sameAsPrefix.matcher(uri);
+                Matcher m = servicePattern.matcher(uri);
 
                 if (m.find()) {
                     if (m.end() < uri.length()) {
                         throw new RuntimeException("Trailing characters found after '" + m.group() + "': " + uri.substring(m.end()));
                     }
                     // Inherit union default graph if backed by tdb
-                    DatasetGraph adhocDs = DatasetGraphUnionDefaultGraph.wrapIfNeeded(execCxt.getDataset());
-                    adhocDs = DatasetGraphSameAsOld.wrap(adhocDs);
+                    DatasetGraph adhocDs = wrapper.apply(execCxt);
                     r = QueryExecUtils.execute(opExec.getSubOp(), adhocDs, binding, execCxt.getContext());
                 } else {
                     r = chain.createExecution(opExec, opOrig, binding, execCxt);
