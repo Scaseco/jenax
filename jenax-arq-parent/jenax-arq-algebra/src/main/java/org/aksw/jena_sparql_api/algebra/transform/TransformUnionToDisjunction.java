1c82697d3c533cc710dc18bae2e2c271be1101ba
Claus Stadler <RavenArkadon@googlemail.com>
2023-02-27 18:15:31 +0100

    Improved union to disjunction transformer (I couldn't find it in Jena but probably it exists)

--- jenax-arq-parent/jenax-arq-algebra/src/main/java/org/aksw/jena_sparql_api/algebra/transform/TransformUnionToDisjunction.java
+++ jenax-arq-parent/jenax-arq-algebra/src/main/java/org/aksw/jena_sparql_api/algebra/transform/TransformUnionToDisjunction.java
@@ -1,25 +1,38 @@
 package org.aksw.jena_sparql_api.algebra.transform;
 
+import java.util.List;
+
 import org.apache.jena.sparql.algebra.Op;
 import org.apache.jena.sparql.algebra.TransformCopy;
 import org.apache.jena.sparql.algebra.op.OpDisjunction;
 import org.apache.jena.sparql.algebra.op.OpUnion;
 
 public class TransformUnionToDisjunction
-    extends TransformCopy 
-{    
+    extends TransformCopy
+{
     public static final TransformUnionToDisjunction fn = new TransformUnionToDisjunction();
 
     @Override
     public Op transform(OpUnion opUnion, Op left, Op right) {
         OpDisjunction result = OpDisjunction.create();
-        
         add(result, left);
         add(result, right);
-
-        return result;        
+        return result;
     }
-    
+
+    @Override
+    public Op transform(OpDisjunction opDisjunction, List<Op> elts) {
+        OpDisjunction result = OpDisjunction.create();
+        for (Op elt : elts) {
+            add(result, elt);
+        }
+        return result;
+    }
+
+    /**
+     * This method does NOT recursively collect.
+     * It only adds the immediate children because the transformer operates bottom up
+     */
     public static void add(OpDisjunction dest, Op op) {
         if(op instanceof OpDisjunction) {
             OpDisjunction o = (OpDisjunction)op;
