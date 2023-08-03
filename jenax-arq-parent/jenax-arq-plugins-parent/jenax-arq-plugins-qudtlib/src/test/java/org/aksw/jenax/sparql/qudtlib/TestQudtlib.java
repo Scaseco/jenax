1a37f198ecd2c0d094c534b267d9f1f2db92a3c1
Simon Bin <sbin@informatik.uni-leipzig.de>
2023-08-03 17:21:19 +0200

    fix qudtlib test

--- jenax-arq-parent/jenax-arq-plugins-parent/jenax-arq-plugins-qudtlib/src/test/java/org/aksw/jenax/sparql/qudtlib/TestQudtlib.java
+++ jenax-arq-parent/jenax-arq-plugins-parent/jenax-arq-plugins-qudtlib/src/test/java/org/aksw/jenax/sparql/qudtlib/TestQudtlib.java
@@ -5,7 +5,7 @@ import org.apache.jena.shared.impl.PrefixMappingImpl;
 import org.apache.jena.sparql.expr.NodeValue;
 import org.apache.jena.sparql.util.ExprUtils;
 import org.apache.jena.sys.JenaSystem;
-import org.junit.Ignore;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestQudtlib {
@@ -16,8 +16,7 @@ public class TestQudtlib {
         pm.setNsPrefix("qudt", Functions.NS);
         pm.setNsPrefix("unit", "http://qudt.org/vocab/unit/");
         NodeValue nv = ExprUtils.eval(ExprUtils.parse("qudt:convert(38.5, unit:DEG_C, unit:DEG_F)", pm));
-        System.out.println(nv);
-
+        Assert.assertEquals("Conversion from C to F", 101.3, nv.getDouble(), 0.0005);
     }
 
     @Test
@@ -26,8 +25,7 @@ public class TestQudtlib {
         pm.setNsPrefix("qudt", Functions.NS);
         pm.setNsPrefix("unit", "http://qudt.org/vocab/unit/");
         NodeValue nv = ExprUtils.eval(ExprUtils.parse("qudt:registeredUnit(unit:DEG_F)", pm));
-        System.out.println(nv);
-
+        Assert.assertTrue("F Unit is registered", nv.getBoolean());
     }
 
 
@@ -37,17 +35,16 @@ public class TestQudtlib {
         pm.setNsPrefix("qudt", Functions.NS);
         pm.setNsPrefix("unit", "http://qudt.org/vocab/unit/");
         NodeValue nv = ExprUtils.eval(ExprUtils.parse("qudt:convertible(unit:DEG_F, unit:M)", pm));
-        System.out.println(nv);
-
+        Assert.assertFalse("Degree F is not convertible to Metres", nv.getBoolean());
     }
 
     @Test
-    @Ignore
     public void testUnitFromLabel() {
         PrefixMapping pm = new PrefixMappingImpl();
         pm.setNsPrefix("qudt", Functions.NS);
 //        pm.setNsPrefix("unit", "http://qudt.org/vocab/unit/");
-        NodeValue nv = ExprUtils.eval(ExprUtils.parse("qudt:unitFromLabel(\"mm2\")", pm));
-        System.out.println(nv);
+        NodeValue nv = ExprUtils.eval(ExprUtils.parse("qudt:unitFromLabel(\"Square Millimetre\")", pm));
+        Assert.assertEquals("The square millimetre unit is found by full English label",
+                "http://qudt.org/vocab/unit/MilliM2", nv.getNode().getURI());
     }
 }
