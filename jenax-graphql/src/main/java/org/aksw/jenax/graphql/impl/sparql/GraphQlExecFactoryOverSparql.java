f910069e1cd5dd7c7d553496a51dde5f9e215c73
Claus Stadler <RavenArkadon@googlemail.com>
2023-10-04 22:41:49 +0200

    Added log output to autodetection

--- jenax-graphql/src/main/java/org/aksw/jenax/graphql/impl/sparql/GraphQlExecFactoryOverSparql.java
+++ jenax-graphql/src/main/java/org/aksw/jenax/graphql/impl/sparql/GraphQlExecFactoryOverSparql.java
@@ -1,5 +1,6 @@
 package org.aksw.jenax.graphql.impl.sparql;
 
+import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
@@ -7,6 +8,9 @@ import org.aksw.jenax.connection.datasource.RdfDataSource;
 import org.aksw.jenax.graphql.GraphQlExec;
 import org.aksw.jenax.graphql.GraphQlExecFactory;
 import org.aksw.jenax.graphql.impl.core.GraphQlExecFactoryLazy;
+import org.apache.jena.graph.Node;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
@@ -18,7 +22,7 @@ import graphql.language.Document;
 public class GraphQlExecFactoryOverSparql
     implements GraphQlExecFactory
 {
-    // private static final Logger logger = LoggerFactory.getLogger(GraphQlExecFactoryOverSparql.class);
+    private static final Logger logger = LoggerFactory.getLogger(GraphQlExecFactoryOverSparql.class);
 
     protected RdfDataSource dataSource;
     protected GraphQlToSparqlConverter converter;
@@ -72,7 +76,19 @@ public class GraphQlExecFactoryOverSparql
                     MoreExecutors.getExitingExecutorService((ThreadPoolExecutor)Executors.newCachedThreadPool()));
 
             ListenableFuture<DatasetMetadata> metadataFuture = DatasetMetadata.fetch(dataSource, executorService);
-            ListenableFuture<GraphQlExecFactory> r = Futures.transform(metadataFuture, metadata -> of(dataSource, metadata), executorService);
+            // TODO Make display of auto-detection results optional
+            ListenableFuture<GraphQlExecFactory> r = Futures.transform(metadataFuture, metadata -> {
+                if (logger.isInfoEnabled()) {
+                    Set<Node> classes = metadata.getVoidDataset().getClassPartitionMap().keySet();
+                    logger.info("Autodetected classes: " + classes);
+
+                    Set<Node> properties = metadata.getVoidDataset().getPropertyPartitionMap().keySet();
+                    logger.info("Autodetected properties: " + properties);
+                }
+
+                GraphQlExecFactory s = of(dataSource, metadata);
+                return s;
+            }, executorService);
 
             r.addListener(() -> executorService.shutdown(), executorService);
             return r;
