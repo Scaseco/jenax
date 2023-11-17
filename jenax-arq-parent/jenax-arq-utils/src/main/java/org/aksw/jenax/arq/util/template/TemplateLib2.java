2915a8f898a61394231187de1b35bd5d02be3aeb
Claus Stadler <RavenArkadon@googlemail.com>
2023-11-17 18:18:13 +0100

    Added TemplateUtils

---
+++ jenax-arq-parent/jenax-arq-utils/src/main/java/org/aksw/jenax/arq/util/template/TemplateLib2.java
@@ -0,0 +1,54 @@
+package org.aksw.jenax.arq.util.template;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.jena.atlas.iterator.Iter;
+import org.apache.jena.graph.Triple;
+import org.apache.jena.sparql.core.Quad;
+import org.apache.jena.sparql.core.Substitute;
+import org.apache.jena.sparql.engine.binding.Binding;
+import org.apache.jena.sparql.modify.TemplateLib;
+
+/** Similar to Jena's {@link TemplateLib} but without remapping of blank nodes */
+public class TemplateLib2 {
+    public static Iterator<Quad> mapToQuads(Quad tuple, Iterator<Binding> bindings) {
+        return Iter.iter(bindings)
+                .map(binding -> {
+                    Quad r = Substitute.substitute(tuple, binding);
+                    return r;
+                })
+                .filter(Quad::isConcrete);
+    }
+
+    public static Iterator<Quad> mapToQuads(List<Quad> tuples, Iterator<Binding> bindings) {
+        return Iter.iter(bindings)
+                .flatMap(binding -> Iter.iter(tuples)
+                        .map(tuple -> {
+                            Quad r = Substitute.substitute(tuple, binding);
+                            return r;
+                        })
+                        .filter(Quad::isConcrete)
+                );
+    }
+
+    public static Iterator<Triple> mapToTriples(Triple tuple, Iterator<Binding> bindings) {
+        return Iter.iter(bindings)
+                .map(binding -> {
+                    Triple r = Substitute.substitute(tuple, binding);
+                    return r;
+                })
+                .filter(Triple::isConcrete);
+    }
+
+    public static Iterator<Triple> mapToTriples(List<Triple> tuples, Iterator<Binding> bindings) {
+        return Iter.iter(bindings)
+                .flatMap(binding -> Iter.iter(tuples)
+                    .map(tuple -> {
+                        Triple r = Substitute.substitute(tuple, binding);
+                        return r;
+                    })
+                    .filter(Triple::isConcrete)
+                );
+    }
+}
