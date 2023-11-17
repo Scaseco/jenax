d7ab2cf6a99ca01d804aea1227fb06b0dd95584c
Claus Stadler <RavenArkadon@googlemail.com>
2023-11-17 23:57:15 +0100

    Aligned method names with original TemplateLib

--- jenax-arq-parent/jenax-arq-utils/src/main/java/org/aksw/jenax/arq/util/template/TemplateLib2.java
+++ jenax-arq-parent/jenax-arq-utils/src/main/java/org/aksw/jenax/arq/util/template/TemplateLib2.java
@@ -12,43 +12,43 @@ import org.apache.jena.sparql.modify.TemplateLib;
 
 /** Similar to Jena's {@link TemplateLib} but without remapping of blank nodes */
 public class TemplateLib2 {
-    public static Iterator<Quad> mapToQuads(Quad tuple, Iterator<Binding> bindings) {
+    public static Iterator<Triple> calcTriples(Triple tuple, Iterator<Binding> bindings) {
         return Iter.iter(bindings)
-                .map(binding -> {
-                    Quad r = Substitute.substitute(tuple, binding);
-                    return r;
-                })
-                .filter(Quad::isConcrete);
+            .map(binding -> {
+                Triple r = Substitute.substitute(tuple, binding);
+                return r;
+            })
+            .filter(Triple::isConcrete);
     }
 
-    public static Iterator<Quad> mapToQuads(List<Quad> tuples, Iterator<Binding> bindings) {
+    public static Iterator<Triple> calcTriples(List<Triple> tuples, Iterator<Binding> bindings) {
         return Iter.iter(bindings)
-                .flatMap(binding -> Iter.iter(tuples)
-                        .map(tuple -> {
-                            Quad r = Substitute.substitute(tuple, binding);
-                            return r;
-                        })
-                        .filter(Quad::isConcrete)
-                );
-    }
-
-    public static Iterator<Triple> mapToTriples(Triple tuple, Iterator<Binding> bindings) {
-        return Iter.iter(bindings)
-                .map(binding -> {
+            .flatMap(binding -> Iter.iter(tuples)
+                .map(tuple -> {
                     Triple r = Substitute.substitute(tuple, binding);
                     return r;
                 })
-                .filter(Triple::isConcrete);
+                .filter(Triple::isConcrete)
+            );
     }
 
-    public static Iterator<Triple> mapToTriples(List<Triple> tuples, Iterator<Binding> bindings) {
+    public static Iterator<Quad> calcQuads(Quad tuple, Iterator<Binding> bindings) {
         return Iter.iter(bindings)
-                .flatMap(binding -> Iter.iter(tuples)
+            .map(binding -> {
+                Quad r = Substitute.substitute(tuple, binding);
+                return r;
+            })
+            .filter(Quad::isConcrete);
+    }
+
+    public static Iterator<Quad> calcQuads(List<Quad> tuples, Iterator<Binding> bindings) {
+        return Iter.iter(bindings)
+            .flatMap(binding -> Iter.iter(tuples)
                     .map(tuple -> {
-                        Triple r = Substitute.substitute(tuple, binding);
+                        Quad r = Substitute.substitute(tuple, binding);
                         return r;
                     })
-                    .filter(Triple::isConcrete)
-                );
+                    .filter(Quad::isConcrete)
+            );
     }
 }
