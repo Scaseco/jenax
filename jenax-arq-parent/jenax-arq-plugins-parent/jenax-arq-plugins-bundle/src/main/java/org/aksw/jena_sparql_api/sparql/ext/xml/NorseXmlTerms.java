79fcad070d8901f3b6956f795b6fa835009bc394
Claus Stadler <RavenArkadon@googlemail.com>
2024-01-16 16:28:30 +0100

    fixed typo

--- jenax-arq-parent/jenax-arq-plugins-parent/jenax-arq-plugins-bundle/src/main/java/org/aksw/jena_sparql_api/sparql/ext/xml/NorseXmlTerms.java
+++ jenax-arq-parent/jenax-arq-plugins-parent/jenax-arq-plugins-bundle/src/main/java/org/aksw/jena_sparql_api/sparql/ext/xml/NorseXmlTerms.java
@@ -3,7 +3,7 @@ package org.aksw.jena_sparql_api.sparql.ext.xml;
 import org.aksw.jenax.norse.NorseTerms;
 
 public class NorseXmlTerms {
-    public static final String NS = NorseTerms.NS + ".xml";
+    public static final String NS = NorseTerms.NS + "xml.";
 
     public static final String parse = NS + "parse";
     public static final String path = NS + "path";
