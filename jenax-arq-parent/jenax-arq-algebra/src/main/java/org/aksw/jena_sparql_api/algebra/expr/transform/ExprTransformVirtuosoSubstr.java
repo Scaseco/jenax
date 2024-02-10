4f9940a4a0d7378dcfe5fa40b936445f34ad6f4a
Claus Stadler <RavenArkadon@googlemail.com>
2024-02-10 01:50:29 +0100

    Fixed the legacy virtuoso substr polyfill rewrite and added test case

--- jenax-arq-parent/jenax-arq-algebra/src/main/java/org/aksw/jena_sparql_api/algebra/expr/transform/ExprTransformVirtuosoSubstr.java
+++ jenax-arq-parent/jenax-arq-algebra/src/main/java/org/aksw/jena_sparql_api/algebra/expr/transform/ExprTransformVirtuosoSubstr.java
@@ -6,7 +6,6 @@ import org.aksw.commons.util.list.ListUtils;
 import org.aksw.commons.util.obj.ObjectUtils;
 import org.apache.jena.sparql.expr.E_Conditional;
 import org.apache.jena.sparql.expr.E_GreaterThan;
-import org.apache.jena.sparql.expr.E_GreaterThanOrEqual;
 import org.apache.jena.sparql.expr.E_StrLength;
 import org.apache.jena.sparql.expr.E_StrSubstring;
 import org.apache.jena.sparql.expr.E_Subtract;
@@ -17,8 +16,10 @@ import org.apache.jena.sparql.expr.ExprTransformCopy;
 import org.apache.jena.sparql.expr.NodeValue;
 
 /**
- * Fixes substr(str, start, len) of virtuoso which does not
- * restrict the substring to the length of the string.
+ * Fixes substr(str, start, len) of virtuoso which raises an error if the
+ * requested substring is longer than the string.
+ *
+ * Note: This rewrite does not handle the cases where start < 1 or len < 0
  */
 public class ExprTransformVirtuosoSubstr
     extends ExprTransformCopy
@@ -30,18 +31,40 @@ public class ExprTransformVirtuosoSubstr
         if (e != null) {
             List<Expr> argList = args.getList();
             Expr str = ListUtils.getOrNull(argList, 0);
-            Expr offset = ListUtils.getOrNull(argList, 1);
+            Expr start = ListUtils.getOrNull(argList, 1);
             Expr len = ListUtils.getOrNull(argList, 2);
 
-            // if (?offset >= strlen(?str), "",
-            //     substr(?str, ?offset,
-            //         if (?len >= strlen(?str) - ?offset, ?strlen(?str) - ?len, ?len)))
+            // If we wanted to handle offsets < 1 we'd need:
+            // Adjusted offset: if (?offset < 1 ? 1 : ?offset)
+            // shift: if (?offset >= 1 ? 0 : 1 - ?offset)
+
             Expr strLen = new E_StrLength(str);
 
-            // TODO Negative values for offsets and length not handled yet
-            result = new E_Conditional(new E_GreaterThanOrEqual(offset, strLen), NodeValue.makeString(""),
-                    new E_StrSubstring(str, offset, new E_Conditional(new E_GreaterThanOrEqual(len, new E_Subtract(strLen, offset)),
-                            new E_Subtract(strLen, offset), len)));
+            // zero-based-start = (?start - 1)
+            // remaining length = strlen(?str) - zero-based-start
+            Expr start0 = new E_Subtract(start, NodeValue.nvONE);
+            Expr remainingLen = new E_Subtract(strLen, start0);
+
+            if (len == null) {
+                // if (?start > strlen(?str), "",
+                //     substr(?str, ?start, ?remaining)))
+
+                result = new E_Conditional(new E_GreaterThan(start, strLen), NodeValue.makeString(""),
+                        new E_StrSubstring(str, start, remainingLen));
+
+            } else {
+                // if (?offset > strlen(?str), "",
+                //     substr(?str, ?start,
+                //         if (?len > ?remaining, ?remaining, ?len)))
+
+                result = new E_Conditional(new E_GreaterThan(start, strLen), NodeValue.makeString(""),
+                        new E_StrSubstring(str, start, new E_Conditional(new E_GreaterThan(len, remainingLen),
+                                remainingLen, len)));
+            }
+
+            // Constant-folding should be done extrinsically
+            // result = ExprTransformer.transform(new ExprTransformConstantFold(), result);
+
         } else {
             result = super.transform(func, args);
         }
