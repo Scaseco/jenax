ce20c6694f63b536d927831ea61cd19146ed320b
Claus Stadler <RavenArkadon@googlemail.com>
2024-05-10 14:58:02 +0200

    Moved (most of) Skolemize from LSQ to JenaX Added JenaXSymbols with symResourceMgr as a proposed base infrastructure for closing resources after query exec.

---
+++ jenax-arq-parent/jenax-arq-utils/src/main/java/org/aksw/jenax/arq/util/exec/query/JenaXSymbols.java
@@ -0,0 +1,13 @@
+package org.aksw.jenax.arq.util.exec.query;
+
+import org.apache.jena.sparql.util.Symbol;
+
+public class JenaxSymbols {
+    /**
+     * Symbol for a jenax-based ResourceManager.
+     * Certain JenaX components (such as the JDBC extensions for RML) need to
+     * dynamically allocate resources during query execution that eventually need to be freed.
+     * These components will check the Context for a resource manager and register the clean up actions there.
+     */
+    public static final Symbol symResourceMgr = Symbol.create("JenaXResourceMgr");
+}
