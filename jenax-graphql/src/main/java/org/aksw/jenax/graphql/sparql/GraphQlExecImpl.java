d02c76fef6ea26995dae4cd4cd702af9b78ed9be
Claus Stadler <RavenArkadon@googlemail.com>
2024-06-20 19:47:19 +0200

    GraphQL: Fixed bug in UI when creating curl link with single quotes. Fixed incorrect 'null' json element when SPARQL result is empty.

--- jenax-graphql/src/main/java/org/aksw/jenax/graphql/sparql/GraphQlExecImpl.java
+++ jenax-graphql/src/main/java/org/aksw/jenax/graphql/sparql/GraphQlExecImpl.java
@@ -148,8 +148,8 @@ public class GraphQlExecImpl
 
                     driver.end(context);
 
-                    // Write null if there were no sources
-                    if (driver.getSourcesSeen() == 0) {
+                    // Write null if in single mode and there were no sources
+                    if (isSingle && driver.getSourcesSeen() == 0) {
                         writer.nullValue();
                     }
                 }
