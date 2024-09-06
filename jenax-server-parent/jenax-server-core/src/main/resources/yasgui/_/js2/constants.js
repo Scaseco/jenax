644b117bde1231eb66b6aad91a55012b388ad576
Simon Bin <sbin@informatik.uni-leipzig.de>
2024-09-06 18:57:20 +0200

    add a copy of ldv version 8a299534d3ee22f2b77154763381df24fe159190

---
+++ jenax-server-parent/jenax-server-core/src/main/resources/yasgui/_/js2/constants.js
@@ -0,0 +1,10 @@
+(() => {
+  const ldvDef = {
+    expandButtonText: '[+]',
+    collapseButtonText: '[\u2212]',
+    sourceGraphPropId: 'urn:x-meta:originating-graph',
+    reversePropPrefix: 'urn:x-ldv:reverse',
+    moreResultsObjId: 'urn:x-ldv:more-results',
+  }
+  window.ldvDef = ldvDef
+})()
