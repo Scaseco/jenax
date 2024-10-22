ad3f679003e14a5b9b02bdec05d9e40faa4d7ef4
Claus Stadler <RavenArkadon@googlemail.com>
2024-10-22 13:39:09 +0200

    Registered csv parsing under norse:csv.parse

--- jenax-arq-parent/jenax-arq-plugins-parent/jenax-arq-plugins-bundle/src/main/java/org/aksw/jena_sparql_api/sparql/ext/csv/JenaExtensionCsv.java
+++ jenax-arq-parent/jenax-arq-plugins-parent/jenax-arq-plugins-bundle/src/main/java/org/aksw/jena_sparql_api/sparql/ext/csv/JenaExtensionCsv.java
@@ -1,19 +1,23 @@
 package org.aksw.jena_sparql_api.sparql.ext.csv;
 
+import org.aksw.jenax.norse.term.csv.NorseTermsCsv;
 import org.apache.jena.shared.PrefixMapping;
 import org.apache.jena.sparql.function.FunctionRegistry;
 import org.apache.jena.sparql.pfunction.PropertyFunctionRegistry;
 
 public class JenaExtensionCsv {
     public static final String ns = "http://jsa.aksw.org/fn/csv/";
-    
+
     public static void register() {
+        FunctionRegistry.get().put(NorseTermsCsv.parse, E_CsvParse.class);
         FunctionRegistry.get().put(ns + "parse", E_CsvParse.class);
-        
+
+        PropertyFunctionRegistry.get().put(NorseTermsCsv.parse, new PropertyFunctionFactoryCsvParse());
         PropertyFunctionRegistry.get().put(ns + "parse", new PropertyFunctionFactoryCsvParse());
     }
-    
+
     public static void addPrefixes(PrefixMapping pm) {
-		pm.setNsPrefix("csv", ns);
+        //pm.setNsPrefix("csv", ns);
+        pm.setNsPrefix("csv", NorseTermsCsv.NS);
     }
 }
