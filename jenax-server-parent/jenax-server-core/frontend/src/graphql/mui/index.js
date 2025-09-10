153abb4861b9fcd79ceef0cf733512f6266b6029
Claus Stadler <RavenArkadon@googlemail.com>
2025-09-10 15:35:26 +0200

    fixed javascript base url function.

--- jenax-server-parent/jenax-server-core/frontend/src/graphql/mui/index.js
+++ jenax-server-parent/jenax-server-core/frontend/src/graphql/mui/index.js
@@ -19,7 +19,7 @@ function baseUrl(urlStr) {
   const url = new URL(urlStr);
   url.search = "";
   url.hash = "";
-  return url;
+  return url.toString();
 }
 
 const endpoint = baseUrl(document.location.href);
